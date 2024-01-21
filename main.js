eabcc95449f38723a49c2ecc3fee9b9f08eccfa4
deepto98 <ddepp98@outlook.com>
2024-01-21 21:32:14 +0530

    added initial lift creation, with gaps b/w them

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createLifts() {
     var liftSystemContainer = document.getElementById('liftSystemContainer');
     liftSystemContainer.innerHTML = ''; // Clear previous content
 
-    for (var floor = numFloors; floor >= 1 ; floor--) {
+    for (var floor = numFloors; floor >= 1; floor--) {
         var floorDiv = document.createElement('div');
         floorDiv.classList.add('floor');
 
@@ -52,13 +52,30 @@ function createLifts() {
         floorLabel.textContent = 'Floor ' + floor;
 
         floorDiv.appendChild(buttonContainer);
+        if (floor == 1) {
+
+            var liftContainer = document.createElement('div');
+            liftContainer.classList.add('lift-container');
+
+            for (var lift = 1; lift <= numLifts; lift++) {
+
+                // Create a new div element for the blue rectangle
+                var liftRectangle = document.createElement("div");
+
+                liftRectangle.classList.add('lift');
+                // Set the id of the new div element to "lift"
+                liftRectangle.id = "lift".concat(lift);
+
+                // Append the new div element to the container
+                liftContainer.appendChild(liftRectangle);
+            }
+            blackLine.appendChild(liftContainer);
+
+        }
         floorDiv.appendChild(blackLine);
+
         floorDiv.appendChild(floorLabel);
 
-        liftSystemContainer.appendChild(floorDiv);
+        liftSystemContainer.appendChild(floorDiv)
     }
-
-    console.log('Number of Floors:', numberOfFloors);
-    console.log('Number of Lifts:', numberOfLifts);
-
 }
