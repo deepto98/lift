aed8ccfcb5e3fb238696a0089e89773baf2e6ec8
Deeptonabho Dutta <ddepp98@outlook.com>
2024-02-02 05:28:53 +0530

    completed upPressed

--- lifts.js
+++ lifts.js
@@ -25,18 +25,18 @@ function createLifts() {
 
         var ulElement = document.createElement("ul");
 
-        if (floor != numFloors){
-        var liElement = document.createElement("li");
+        if (floor != numFloors) {
+            var liElement = document.createElement("li");
 
 
-        var upButton = document.createElement('button');
-        upButton.classList.add('up-button');
-        upButton.textContent = 'Up';
-        upButton.id = "up-btn-".concat(floor);
+            var upButton = document.createElement('button');
+            upButton.classList.add('up-button');
+            upButton.textContent = 'Up';
+            upButton.id = "up-btn-".concat(floor);
 
-        liElement.appendChild(upButton)
+            liElement.appendChild(upButton)
 
-        ulElement.appendChild(liElement)
+            ulElement.appendChild(liElement)
         }
         // var separator = document.createElement('div');
         // separator.classList.add('separator');
@@ -94,11 +94,14 @@ function createLifts() {
     // Manage positions here
     let positions = []; // this array stores positions of each lift
     // Fill values with 1 indexing, position[liftNo]=floor
+    positions[0] = 0; // setting 0 nd to 0, for easy destructuring from sessionstorage
     for (let i = 1; i <= numLifts; i++) {
         positions[i] = 1  //Initially all lifts are at floor 1
     }
     // Set as positions key in sessionStorage
     sessionStorage.setItem('positions', positions);
+    sessionStorage.setItem('numLifts', numLifts);
+    sessionStorage.setItem('numFloors', numFloors);
 
     // Get the button element by its id
     var upButtons = document.getElementsByClassName("up-button");
@@ -109,47 +112,115 @@ function createLifts() {
     for (const upButton of upButtons) {
         // Add an event listener for the "click" event
         let floor = upButton.id.slice(7) // ids are in the format up-btn-2
-        upButton.addEventListener("click", toggleUpAnimation);
+        upButton.addEventListener("click", upPressed);
         upButton.floor = floor
     }
 
     for (const downButton of downButtons) {
         // Add an event listener for the "click" event
         let floor = upButton.id.slice(7) // ids are in the format up-btn-2
-        downButton.addEventListener("click", toggleDownAnimation);
+        downButton.addEventListener("click", downPressed);
         downButton.floor = floor
     }
 }
 
-function toggleUpAnimation(event) {
-    floorFromWhichCalled = event.currentTarget.floor
+function getNearestLift(currentFloor) {
+    let positions = parsePositionsFromSessionStorage();
+    let numFloors = sessionStorage.getItem('numFloors');
+    let numLifts = sessionStorage.getItem('numLifts');
+
+    let leastDistFromCurrentFloor = numFloors
+    let nearestLift = numLifts + 1
+    for (let liftNo = 1; liftNo <= positions.length; liftNo++) {
+        if (Math.abs(positions[liftNo] - currentFloor) < leastDistFromCurrentFloor) {
+            leastDistFromCurrentFloor = Math.abs(positions[liftNo] - currentFloor)
+            nearestLift = liftNo
+        }
+    }
+
+    return nearestLift
+}
+
+function parsePositionsFromSessionStorage(){
+    var positions = sessionStorage.getItem('positions');
+    var positionsArr = positions.split(',');
+    return positionsArr
+}
+
+function upPressed(event) {
     // Animations
-    console.log("floorFromWhichCalled")
-    console.log(floorFromWhichCalled)
+
     // Calculate lift to move
     // Up can be called from 1st floor to numFloors-1 th floor 
     // First we need to find the nearest lift
+    // If a lift is present at that floor, do nothing
+    floorFromWhichCalled = event.currentTarget.floor
+    console.log("floorFromWhichCalled")
+    console.log(floorFromWhichCalled)
+
+    // Get nearest lift
+    let nearestLift = getNearestLift(floorFromWhichCalled)
+    console.log("nearestLift")
+    console.log(nearestLift)
+    // window.alert(nearestLift)
+
     // Fetch positions
-    var positions = sessionStorage.getItem('positions');
+    var positions = parsePositionsFromSessionStorage();
     console.log("Current positions")
     console.log(positions)
-    var animatedDiv = document.getElementById("lift-2");
-    // console.log(animatedDiv)
-    var direction = -1; // 1 for moving down, -1 for moving up
-    var distance = 130; // Adjust the speed of the animation
-    var isAnimating = false;
-    if (isAnimating) {
-        // Stop the animation
-        clearInterval(animationInterval);
-    } else {
-        // Start the animation
-        animationInterval = setInterval(moveDiv(animatedDiv, direction, distance), 16); // Adjust the interval for smoother animation (16ms = 60fps)
-    }
+    console.log("Current positions 1")
+    console.log(positions[1])
+    let floorOfNearestLift = positions[nearestLift];
+    console.log("floorOfNearestLift")
+    console.log(floorOfNearestLift)
 
-    isAnimating = !isAnimating;
+    var liftToMove = document.getElementById("lift-".concat(nearestLift));
+    console.log("liftToMove")
+    console.log(liftToMove)
+    //    if lift is on same floor, do nothing
+    if (floorOfNearestLift == floorFromWhichCalled) {
+
+    }
+    //lift is above, has to move down
+    else if (floorOfNearestLift > floorFromWhichCalled) {
+        var direction = 1; // 1 for moving down, -1 for moving up
+        var distance = 130 * (floorOfNearestLift - floorFromWhichCalled); // Adjust the speed of the animation
+        var isAnimating = false;
+        if (isAnimating) {
+            // Stop the animation
+            clearInterval(animationInterval);
+        } else {
+            // Start the animation
+            animationInterval = setInterval(moveDiv(liftToMove, direction, distance), 16); // Adjust the interval for smoother animation (16ms = 60fps)
+        }
+
+        isAnimating = !isAnimating;
+    }
+    //lift is below, has to move up
+    else if (floorOfNearestLift < floorFromWhichCalled) {
+        var direction = -1; // 1 for moving down, -1 for moving up
+        var distance = 130 * (floorFromWhichCalled - floorOfNearestLift); // Adjust the speed of the animation
+        var isAnimating = false;
+        if (isAnimating) {
+            // Stop the animation
+            clearInterval(animationInterval);
+        } else {
+            // Start the animation
+            animationInterval = setInterval(moveDiv(liftToMove, direction, distance), 16); // Adjust the interval for smoother animation (16ms = 60fps)
+        }
+
+        isAnimating = !isAnimating;
+    }
+    positions[nearestLift] = floorFromWhichCalled
+
+    // let positionscp =positions 
+    // positionscp[1]=2
+    sessionStorage.setItem('positions', positions);
+    // console.log("New Current positions")
+    // console.log(positionscp)
 }
 
-function toggleDownAnimation(event) {
+function downPressed(event) {
     floorFromWhichCalled = event.currentTarget.floor
     // Animations
     var animatedDiv = document.getElementById("lift-2");
